6d832aed3e8ff13331fc5dadbefcd6bc5aaf4d6a
MasaKusuda <masa.kusuda.1999@gmail.com>
2021-09-27 23:58:17 +0900

    状態が送信されていなかった問題を修正

--- script.js
+++ script.js
@@ -5,18 +5,14 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 function all_turn(){
-    let position = [642,645,648,654,657,660]
+    let position = [642,643,645,646,648,649,654,655,657,658,660,661]
     let box_ids =[]
     position.forEach(function(item,index) {
         box_ids.push('ember'.concat([item]))
     })
     box_ids.forEach(function(item,index){
         let element = document.getElementById(item)
-        if (element.checked){
-            element.checked=false
-        }else{
-            element.checked=true
-        }
+        element.click()
     })
     document.getElementById('ember670').click();
 }
