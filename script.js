b20cd0789cd856bd86a8ebeec3f2e296142768eb
MasaKusuda <masa.kusuda.1999@gmail.com>
2021-09-28 00:15:12 +0900

    changed lambda

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 function all_turn(){
     let position = [642,643,645,646,648,649,654,655,657,658,660,661]
     let box_ids =[]
-    position.forEach(function(item,index) {
+    position.forEach((item,index) => {
         box_ids.push('ember'.concat([item]))
     })
     box_ids.forEach(function(item,index){
